4b42ae7ff48020acc2b5ca9b8ef28f298031b923
Christian Encarnacion <encarc@rpi.edu>
2015-03-17 17:13:58 -0400

    Added Degree App

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'myapp/login.html'}),
 )
