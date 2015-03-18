1d49bdfc71f668d5c522646a404014e05de5534d
ZacRavichandran <ravicz@rpi.edu>
2015-03-18 11:27:19 -0400

    Updates for the Degree model

--- mysite/degree/models.py
+++ mysite/degree/models.py
@@ -1,4 +1,32 @@
+
+
+"Contributor(s): Zachary Ravichandran"
+"Description: Contains model declarations for Degree "
+
+
+# === Imports ===
 from django.db import models
 
-# Create your models here.
-""
+
+# === Model declarations ===
+# Every Degree has a name and shorthand
+# Every Degree has a semester and year created
+
+
+class Degree_Application(models.Model):
+	SEMESTERS = (
+		('SP', 'SPRING')
+		('SM', 'SUMMER')
+		('FL', 'FALL')
+	)
+
+	name = models.CharField(max_length=20, unique=True)
+	shorthand = models.CharField(max_length=5, unique=True)
+	semester = models.CharField(max_length=2, choices = SEMESTERS)
+	year = models.CharField(max_length = 4)
+	slug = models.SlugField(unique=True)
+
+
+
+	def __unicode__(self):
+		return self.name
