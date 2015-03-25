4ec3308294861c08c4f0ed2065f66d0bdf38c917
swood456 <crazedsportsfan@gmail.com>
2015-03-24 22:12:35 -0400

    Completed simple version of Degree models
    simple version of models. Degree Requirement does not yet point to a
    course, and the autopopulate slugs do not work

--- mysite/degree/admin.py
+++ mysite/degree/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from degree.models import Semester, Degree, DegreeRequirement
 
 # Register your models here.
+class SemesterAdmin(admin.ModelAdmin):
+	prepoulated_fields 	= {'slug' : ('season',)}
+	list_display		= ('season', 'year')
+
+class DegreeAdmin(admin.ModelAdmin):
+	prepoulated_fields 	= {'slug' : ('name')}
+	list_display		= ('name', 'shorthand')
+	search_fields		= ['name']
+
+class RequirementAdmin(admin.ModelAdmin):
+	list_display 		= ('course', 'degree', 'semester')
+
+
+admin.site.register(Semester, SemesterAdmin)
+admin.site.register(Degree, DegreeAdmin)
+admin.site.register(DegreeRequirement, RequirementAdmin)
